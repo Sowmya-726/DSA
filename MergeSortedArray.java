3e3c2ae20ae8e6aab65383c1953c9193e408105e
Sowmya-726 <mail2sowmya.g@gmail.com>
2026-05-02 15:07:25 +0530

    Add files via upload

---
+++ MergeSortedArray.java
@@ -0,0 +1,37 @@
+
+/*
+Merge Sorted Array
+
+- nums1 has size m+n (first m elements valid)
+- nums2 has n elements
+- Merge nums2 into nums1 in-place
+
+Time: O(m+n)
+Space: O(1)
+*/
+
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = n+m-1;
+
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
